59026404d91773f3a4ed96fa265a43c8a0258425
RashidAlackal <asrashid4@gmail.com>
2020-05-27 18:48:20 +0530

    Updates

---
+++ precache-manifest.54f75ee8e651c01a36ac2e29de251166.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0d1db076781480394fb698feed2ae2",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "8b9ce9e04487368b0238",
+    "url": "/portfolio-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5d4200587d7334ba9231",
+    "url": "/portfolio-react/static/css/main.92166397.chunk.css"
+  },
+  {
+    "revision": "8b9ce9e04487368b0238",
+    "url": "/portfolio-react/static/js/2.5569296f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/portfolio-react/static/js/2.5569296f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4200587d7334ba9231",
+    "url": "/portfolio-react/static/js/main.aa79de3a.chunk.js"
+  },
+  {
+    "revision": "2f877f91cefff865dab0",
+    "url": "/portfolio-react/static/js/runtime-main.e9f076d0.js"
+  },
+  {
+    "revision": "3082fdd4aae7d323c65244c069f97736",
+    "url": "/portfolio-react/static/media/cv_html-css.3082fdd4.png"
+  },
+  {
+    "revision": "fe23680ab2e1c88e13257005ac4b0e26",
+    "url": "/portfolio-react/static/media/portfolio-react.fe23680a.png"
+  },
+  {
+    "revision": "7a5dfe313009cb56265de3db54aba7b6",
+    "url": "/portfolio-react/static/media/shopping-cart-html-css.7a5dfe31.png"
+  }
+]);
